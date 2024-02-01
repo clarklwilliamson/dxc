4cd0bd14fefa95b3f8988578f98a6e8443c5f9eb
Clark <clarklwilliamson@gmail.com>
2024-02-01 14:18:28 -0800

    make it true, da tru tru

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "dxc_subnet1" {
   vpc_id                  = aws_vpc.dxc_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "dxc-subnet1"
@@ -34,7 +34,7 @@ resource "aws_subnet" "dxc_subnet2" {
   vpc_id                  = aws_vpc.dxc_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b" # Change to your desired availability zone
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "dxc-subnet2"
